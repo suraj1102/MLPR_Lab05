5827689b5a7b5342b3e51b3c588d6003080e48b5
Suraj <38291736+suraj1102@users.noreply.github.com>
2025-02-21 16:18:59 +0530

    Change test_script.py

--- test_script.py
+++ test_script.py
@@ -2,3 +2,15 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import sklearn as sk
+import cv2
+
+# Check if data is loading or not 
+def load_image(img_path):
+    img = cv2.imread(img_path)
+    if img is None:
+        return False
+    return True
+
+images = ["Dr_Shashi_Tharoor.jpg", "Plaksha_Faculty.jpg"]
+for img_path in images:
+    print(load_image(img_path))
